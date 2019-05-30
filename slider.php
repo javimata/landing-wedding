45f54dfc8d1e12197ee98746fdee1ce274277120
javimata <javimata@gmail.com>
2019-05-29 21:16:28 -0500

    update slide 01

--- slider.php
+++ slider.php
@@ -67,6 +67,69 @@
 					</div>
 				</li>
 		
+				<!-- SLIDE 2 -->
+				<li data-index="rs-2" data-transition="random-premium" data-slotamount="default" data-hideafterloop="0" data-hideslideonmobile="off"  data-easein="default" data-easeout="default" data-masterspeed="300"  data-rotate="0" data-saveperformance="off"  data-title="Desarrollo web" data-param1="" data-param2="" data-param3="" data-param4="" data-param5="" data-param6="" data-param7="" data-param8="" data-param9="" data-param10="" data-description="">
+					<!-- MAIN IMAGE -->
+					<img src="images/sliders/slider-01.jpg" alt="Marketing digital" data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" class="rev-slidebg" data-no-retina data-bgparallax="5">
+					<!-- LAYERS -->
+
+					<!-- LAYER NR. 1 -->
+					<h1 class="tp-caption tp-resizeme" 
+						id="slide-1-layer-1" 
+						data-x="center" 
+						data-y="140" 
+						data-width="['700']"
+						data-height="['auto']"
+						data-type="text" 
+						data-frames='[{"delay":300,"speed":1000,"split_direction":"forward","sfxcolor":"#ffffff","sfx_effect":"","frame":"0","from":"x:-50px;opacity:0;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"split_direction":"forward","sfxcolor":"#ffffff","use_text_c":false,"use_bg_c":false,"text_c":"transparent","bg_c":"transparent","sfx_effect":"","frame":"999","to":"opacity:0;","ease":"Power3.easeInOut"}]'
+						data-textAlign="['center','inherit','inherit','inherit']"
+						data-paddingtop="[0,0,0,0]"
+						data-paddingright="[0,0,0,0]"
+						data-paddingbottom="[0,0,0,0]"
+						data-paddingleft="[0,0,0,0]"
+						data-responsive_offset="on" 
+						data-basealign="grid" 
+						style="z-index: 5; min-width: 700px; max-width: 700px; white-space: normal; font-family: 'Lato', serif; font-size: 60px; line-height: 60px; font-weight: 900; color: #ffffff; letter-spacing: 0px; text-align: center;" >WE WILL PLAN OF YOUR DREAMS</h1>
+
+					<!-- LAYER NR. 2 -->
+					<h3 class="tp-caption tp-resizeme" 
+						id="slide-1-layer-2" 
+						data-x="center" 
+						data-y="270" 
+						data-width="['auto']"
+						data-height="['auto']"
+						data-type="text" 
+						data-frames='[{"delay":600,"speed":1000,"split_direction":"forward","sfxcolor":"#ffffff","sfx_effect":"","frame":"0","from":"x:-50px;opacity:0;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"split_direction":"forward","sfxcolor":"#ffffff","use_text_c":false,"use_bg_c":false,"text_c":"transparent","bg_c":"transparent","sfx_effect":"","frame":"999","to":"opacity:0;","ease":"Power3.easeInOut"}]'
+						data-textAlign="['inherit','inherit','inherit','inherit']"
+						data-paddingtop="[0,0,0,0]"
+						data-paddingright="[0,0,0,0]"
+						data-paddingbottom="[0,0,0,0]"
+						data-paddingleft="[0,0,0,0]"
+						data-responsive_offset="on" 
+						data-basealign="grid" 
+						style="z-index: 6; white-space: nowrap; font-family: 'Lato', serif; font-size: 30px; line-height: 45px; font-weight: 300; color: #ffffff; letter-spacing: 0px;">PROFESIONAL</h3>
+
+					<!-- LAYER NR. 3 -->
+					<div class="tp-caption" 
+						id="slide-1-layer-3" 
+						data-x="center" 
+						data-y="360" 
+						data-width="['auto']"
+						data-height="['auto']"
+						data-type="text" 
+						data-frames='[{"delay":950,"speed":1000,"split_direction":"forward","sfxcolor":"#ffffff","sfx_effect":"","frame":"0","from":"y:-50px;opacity:0;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"split_direction":"forward","sfxcolor":"#ffffff","use_text_c":false,"use_bg_c":false,"text_c":"transparent","bg_c":"transparent","sfx_effect":"","frame":"999","to":"opacity:0;","ease":"Power3.easeInOut"}]'
+						data-textAlign="['inherit','inherit','inherit','inherit']"
+						data-paddingtop="[0,0,0,0]"
+						data-paddingright="[0,0,0,0]"
+						data-paddingbottom="[0,0,0,0]"
+						data-paddingleft="[0,0,0,0]"
+						data-responsive_offset="on" 
+						data-basealign="grid" 
+						style="z-index: 7; white-space: nowrap; font-size: 30px; line-height: 40px; font-weight: 300; color: #ffffff; letter-spacing: 0px;">
+						<a href="#" class="btn btn-primary btn-bordered">CONOCE MÁS</a>
+					</div>
+				</li>
+
             </ul>
         </div>
     </div>
